13a5980464b095ce2ce068725d343954d1cb24a7
Nathan Stohs <nathan.stohs@gmail.com>
2026-07-07 15:18:53 -0400

    ecg scratch

--- MSenseDevice/src/drivers/nand/nand_disk.c
+++ MSenseDevice/src/drivers/nand/nand_disk.c
@@ -461,7 +461,8 @@ static int disk_sdmmc_init(const struct device *dev)
 	//data->status = SD_UNINIT;
 
 	//spi_nor_data* nand_data = dev->data;
-	LOG_INF("Init disk regist"); 
+	LOG_INF("Init disk regist IGNORED");
+	return 0;
 
 	sdmmc_disk.dev = dev;
 	sdmmc_disk.name = "SD";//dev->name;
